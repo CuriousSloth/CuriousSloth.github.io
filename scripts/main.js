c7c0cc3c62e2c3993baa12e5941ffa7b4988c3d9
CuriousSloth <77039866+CuriousSloth@users.noreply.github.com>
2021-04-21 19:03:09 -0700

    added seconds to the count

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 const timerText = document.getElementById("timer-text");
 
 let count = 0;
-	setInterval(function (){
+
+setInterval(function (){
 	count +=1;
-    timerText.textContent = count;
+    timerText.textContent = count + " seconds";
 }, 1000)
