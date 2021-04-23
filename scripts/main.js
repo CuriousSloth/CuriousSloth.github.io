a37f62d2fb2e04b028c154013a63f0c2cf6532fe
CuriousSloth <77039866+CuriousSloth@users.noreply.github.com>
2021-04-23 15:06:29 -0700

    added a calculator panel

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
 const timerText = document.getElementById("timer-text");
-
 let count = 0;
 
 setInterval(function (){
 	count +=1;
     timerText.textContent = count;
 }, 1000)
+
+//////////////////////////////////////////////////////////////////
+
+const returnValue = document.getElementById('output-text');
+var form = document.getElementById('form')
+let total = 0;
+
+form.addEventListener('submit', function(event){
+    event.preventDefault //prevents the form from autosubmitting
+    var valueOne = document.getElementById('value1');
+    var valueTwo = document.getElementById('value2');    
+    total = valueOne + valueTwo;
+
+    //document.getElementById('output-text').textContent = total;
+    returnValue.textContent = total;
+})
